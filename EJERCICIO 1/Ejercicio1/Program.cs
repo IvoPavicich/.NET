98fa4bc69cb7b199f2e8214a418b238db1e455f1
IvoPavi <ivo.34611@gmail.com>
2022-05-17 12:54:39 -0300

    ejercicio terminado

--- EJERCICIO 1/Ejercicio1/Program.cs
+++ EJERCICIO 1/Ejercicio1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.Clear();
+Console.WriteLine("Por favor ingrese su nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine($"HOLA {nombre}!!");
+Console.WriteLine("¿Desea continuar?");
+var nom = nombre;
+var resp="";
+do {
+    resp = Console.ReadLine();
+    if (resp.ToUpper() == "S")
+    {
+        Console.Clear();
+        Console.WriteLine("Por favor ingrese su nombre");
+        nom = Console.ReadLine();
+        Console.WriteLine($"HOLA {nom}!!");
+        Console.WriteLine("¿Desea continuar?");
+    }
+    if (resp.ToUpper() == "N")
+    {
+        Console.WriteLine("Programa Finalizado Correctamente");
+    }
+    else if(resp.ToUpper()!= "N" && resp.ToUpper() != "S")
+    {
+        Console.WriteLine("no valido");
+        resp= "N";
+    }
+} while (resp.ToUpper()!= "N");
+
+
+
+
+
+
+
