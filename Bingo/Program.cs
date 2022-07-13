d672a615dc4b04714aa2878834066802837d843e
IvoPavi <ivo.34611@gmail.com>
2022-07-13 18:37:10 -0300

    Cartones de Bingo

---
+++ Bingo/Program.cs
@@ -0,0 +1,160 @@
+﻿var carton = new int[3, 9]; 
+var generarNumero = new Random();
+for (int x=0;x<4;x++) 
+{
+    for (int j = 0; j < 9; j++)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            var numero = 0;
+            bool encontrado = false;
+            while (!encontrado)
+            {
+                if (j == 0)
+                {
+                    numero = generarNumero.Next(1, 10);
+                }
+                else if (j == 8)
+                {
+                    numero = generarNumero.Next(80, 91);
+                }
+                else
+                {
+                    numero = generarNumero.Next(j * 10, j * 10 + 10);
+                }
+                encontrado = true;
+                for (int i2 = 0; i2 < 3; i2++)
+                {
+                  if (carton[i2, j] == numero)
+                  {
+                    encontrado = false;
+                    break;
+                  }
+                }
+            }
+            carton[i, j] = numero;
+        }
+    }
+    ////////////////ORDENAMIENTO DE LAS COLUMNAS DE MENOR A MAYOR//////////////
+    for (int j = 0; j < 9; j++)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int k = i + 1; k < 3; k++)
+            {
+                if (carton[i, j]==carton[k, j])
+                {
+                    int aux = carton[i, j];
+                    carton[i, j] = carton[k, j];
+                    carton[k, j] = aux;
+                }
+            }
+        }
+    }
+    
+    var ceros = 0;
+    while (ceros<12) 
+    {
+        var fila = generarNumero.Next(0, 3);
+        var columna = generarNumero.Next(0, 9);
+        if (carton[fila, columna] == 0) 
+        {
+            continue;
+        }
+
+        /*cantidad de ceros en cada fila*/
+        var cerosEnFilas = 0;
+        for (int j = 0; j < 9; j++) 
+        {
+            if (carton[fila,j]==0) 
+            {
+                cerosEnFilas++;
+            }
+        }
+        /*cantidad de ceros en cada columna*/
+        
+        var cerosEnColumnas = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            if (carton[i,columna] == 0)
+            {
+                cerosEnColumnas ++;
+            }
+        }
+
+        /*Cantidad de numeros diferentes de Cero en cada columna*/
+        var noCerosEnColumnas = new int[9];
+        var columnasConSoloNumero = 0;
+        for (int j = 0; j < 9; j++) 
+        {
+            noCerosEnColumnas[j] = 0;
+            for (int i = 0; i < 3; i++) 
+            {
+                if (carton[i,j]!=0) 
+                {
+                    noCerosEnColumnas[j]++;
+                }
+            }
+            if (noCerosEnColumnas[j] == 1)
+            {
+                columnasConSoloNumero++;
+            }
+        }
+
+        /*Columnas que solo tienen un numero distinto de cero*/
+        //var columnasConSoloNumero = 0;
+        //for (int j = 0; j < 9; j++) 
+        //{
+        //    if (noCerosEnColumnas[j] == 1) 
+        //    {
+        //        columnasConSoloNumero++;
+        //    }
+        //}
+        
+        if (cerosEnFilas==4 || cerosEnColumnas==2) 
+        {
+            continue;
+        }
+        if (columnasConSoloNumero==3 && noCerosEnColumnas[columna]!=3) 
+        {
+            continue;
+        }
+
+        carton[fila, columna] = 0;
+        ceros++;
+    }
+
+/////////////////////////////IMPRESION DE LOS CARTONES//////////////////////////
+    Console.WriteLine("----------------------------");
+    for (int i = 0; i < 3; i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < 9; j++)
+        {
+            if (j == 0 && carton[i, j] != 0) {
+                Console.Write($"0{carton[i, j]}");
+            }
+            else if(j == 0) 
+            {
+              Console.Write($"  ");
+            }
+            else
+            {
+                Console.Write("|");
+                if (carton[i, j] == 0)
+                {
+                    Console.Write("  ");
+                }
+                else 
+                {
+                    Console.Write($"{carton[i, j]}");
+                }
+                
+            }
+        }
+        Console.Write("|");
+        Console.Write("\n");
+    }
+    Console.WriteLine("----------------------------");
+    Console.Write("\n");
+}
---
+++ EJERCICIO 9.2/Program.cs
@@ -0,0 +1,106 @@
+﻿//1)    Cartón de 3 filas por 9 columnas
+//2)    El cartón debe tener 15 números y 12 espacios en blanco.
+//3)    Cada fila debe tener 5 números.
+//4)    Cada columna debe tener 1 o 2 números.
+//5)    Ningún número puede repetirse.
+//6)    La primer columna contiene los números del 1 al 9,
+//      la segunda del 10 al 19, la tercera del 20 al 29, 
+//      así sucesivamente hasta la última columna la cual contiene del 80 al 90.
+//7)    Mostrar el carton por pantalla.
+
+int[,] matriz = new int[3, 9];
+Random datos = new Random();
+for (int j=0;j<9;j++) 
+{
+    for (int i = 0; i <3; i++) 
+    {
+        if (j == 0)
+        {
+            int x = datos.Next(1, 10);
+            
+            
+        
+        }
+        else if (j == 1)
+        {
+            int x = datos.Next(10, 20);
+            matriz[i, j] = x;
+
+        }
+        else if (j == 2)
+        {
+            int x = datos.Next(20, 30);
+            matriz[i, j] = x;
+
+        }
+        else if (j == 3)
+        {
+            int x = datos.Next(30, 40);
+            matriz[i, j] = x;
+
+        }
+        else if (j == 4)
+        {
+            int x = datos.Next(40, 50);
+            matriz[i, j] = x;
+
+        }
+        else if (j == 5)
+        {
+            int x = datos.Next(50, 60);
+            matriz[i, j] = x;
+
+        }
+        else if (j == 6)
+        {
+            int x = datos.Next(60, 70);
+            matriz[i, j] = x;
+
+        }
+        else if (j == 7)
+        {
+            int x = datos.Next(70, 80);
+            matriz[i, j] = x;
+
+        }
+        else if (j == 8)
+        {
+            int x = datos.Next(80, 91);
+            matriz[i, j] = x;
+
+        }
+    }
+
+}
+int a = 1;
+bool ordenado = false;
+while ((a < 3) && (ordenado == false)) 
+{
+    int aux;
+    ordenado = true;
+    for (int j = 0; j < 9; j++) 
+    {
+        for (int i=0;i<=a-3;i++) 
+        {
+            if (matriz[i, j] > matriz[i,j+1]) 
+            {
+                ordenado = false;
+                aux = matriz[i, j];
+                matriz[i, j]= matriz[i, j + 1];
+                matriz[i, j + 1] = aux;
+            }  
+        }
+    }
+    a += 1;
+
+}
+
+for (int i=0;i<3;i++)
+{
+    Console.WriteLine("\n");
+    for (int j=0;j<9;j++) 
+    {
+        Console.Write("\t"+matriz[i, j]);
+    }
+}
+
